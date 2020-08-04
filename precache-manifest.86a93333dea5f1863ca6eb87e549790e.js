1452524bf9cf475647a8822e7c9b1ab82c45c157
Tim <tborg84@gmail.com>
2020-08-04 13:23:44 +1000

    Updates

---
+++ precache-manifest.86a93333dea5f1863ca6eb87e549790e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e178fbf5660534538203343eb832477c",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "67af9a361b94e9ab709f",
+    "url": "/ToDo-App/static/css/main.2a0f8cde.chunk.css"
+  },
+  {
+    "revision": "dbc0efa5020677fb70e9",
+    "url": "/ToDo-App/static/js/2.2a78a2db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-App/static/js/2.2a78a2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67af9a361b94e9ab709f",
+    "url": "/ToDo-App/static/js/main.cb0b4fa5.chunk.js"
+  },
+  {
+    "revision": "30c9b0f5949ec99e6a83",
+    "url": "/ToDo-App/static/js/runtime-main.44de1e20.js"
+  }
+]);
